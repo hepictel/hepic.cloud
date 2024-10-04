0ef41285f6af99970f22ed1c5fcaa7d01f9c2d65
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2024-10-04 20:20:37 +0200

    Update Docs.tsx

--- src/components/docs/Docs.tsx
+++ src/components/docs/Docs.tsx
@@ -14,6 +14,7 @@ const Docs = () => {
                         tagline: "Next-Gen Telecom Observability",
                         actions: [
                             { text: "About Us", link: "/about", theme: "alt" },
+                            { text: "Features", link: "/features", theme: "alt" },
                             { text: "Book a Demo", link: "/signup", theme: "brand" },
                         ],
                         image: {
