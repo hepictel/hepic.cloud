6d639e68ce831c3c718eed49d5953b0fed848d6e
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2024-10-04 19:51:47 +0200

    Update rspress.config.ts

--- rspress.config.ts
+++ rspress.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
     title: 'hepic.cloud',
   icon: "/hepic_cloud_logo.png",
   logo: {
-    light: "/hepic_cloud.svg",
-    dark: "/hepic_cloud.svg",
+    light: "/hepic_logo.svg",
+    dark: "/hepic_logo.svg",
   },
   description: 'hepic.cloud: telecom observability',
   
