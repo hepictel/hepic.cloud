ca6c1d2ee9506ca4cd55f8c70f9e4d75bd069474
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2024-10-04 19:39:19 +0200

    Update Docs.tsx

--- src/components/docs/Docs.tsx
+++ src/components/docs/Docs.tsx
@@ -9,7 +9,7 @@ const Docs = () => {
                     title: "hepic.cloud",
                     titleSuffix: "Next-Gen VoIP Monitoring & Troubleshooting",
                     hero: {
-                        name: "",
+                        name: "hepic.",
                         text: "<p className='text-3xl'>Telecom Observability</p>",
                         tagline: "Next-Gen VoIP/RTC Monitoring",
                         actions: [
