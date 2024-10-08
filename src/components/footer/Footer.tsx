c51885de534aae18b0e821171cee7c9911d70699
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2024-10-08 18:20:28 +0200

    Update Footer.tsx

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -7,11 +7,11 @@ export default function Footer () {
     <footer>
         <Cookie/>
             <div className="border border-x-0 border-b-0 border-t-1 border-[var(--footer-border)] mt-6 pt-4 flex justify-between flex-wrap">
-        <p>&copy; 2024 HEPvest Holding BV</p>
+        <p>&copy; 2024 QXIP BV</p>
         <div>
-          <a href="/legal" className="text-slate-50 mr-4">Terms</a>
+          <!-- <a href="/legal" className="text-slate-50 mr-4">Terms</a> -->
           <a href="/legal/cookies" className="text-slate-50 mr-4">Cookies</a>
-          <a href="mailto:info@gigapipe.com" className="text-slate-50 mr-4">Contact Us</a>
+          <a href="mailto:info@qxip.net" className="text-slate-50 mr-4">Contact Us</a>
         </div>
       </div>
     </footer>
