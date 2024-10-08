d5541af658340eb9b86670cb2ccb21cc35d73251
Joel Guerra <jacovinus@gmail.com>
2024-10-08 18:23:42 +0200

    fix: rm comment

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -9,7 +9,6 @@ export default function Footer () {
             <div className="border border-x-0 border-b-0 border-t-1 border-[var(--footer-border)] mt-6 pt-4 flex justify-between flex-wrap">
         <p>&copy; 2024 QXIP BV</p>
         <div>
-          <!-- <a href="/legal" className="text-slate-50 mr-4">Terms</a> -->
           <a href="/legal/cookies" className="text-slate-50 mr-4">Cookies</a>
           <a href="mailto:info@qxip.net" className="text-slate-50 mr-4">Contact Us</a>
         </div>
